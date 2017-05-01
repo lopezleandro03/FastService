ca44767e511a371a371b6fe0c093800ddcac9cce
lopezleandro03 <lopezleandro03@gmail.com>
2017-04-30 22:37:55 -0300

    Enable eager

--- FastService/Controllers/HomeController.cs
+++ FastService/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
 ﻿using FastService.Models.Login;
 using Model.Model;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Backend;
 
 namespace FastService.Controllers
 {
@@ -35,63 +31,36 @@ namespace FastService.Controllers
             else
             {
                 var user = System.Web.HttpContext.Current.Session["USER"] as string;
+                
+                var model = (from x in _dbContext.UsuarioRol
+                             join u in _dbContext.Usuario on x.UserId equals u.UserId
+                             join r in _dbContext.Role on x.RolId equals r.RolId
+                             where u.Email == user
+                             select new MenuModel()
+                             {
+                                 Nombre = u.Nombre,
+                                 Apellido = u.Apellido,
+                                 DefaultController = r.DefaultController,
+                                 DefaultAction = r.DefaultAction,
+                                 MenuItems = (from rm in _dbContext.RoleMenu
+                                              join i in _dbContext.ItemMenu on rm.ItemMenuId equals i.ItemMenuId
+                                              where rm.RolId == x.RolId
+                                              select new MenuItemModel()
+                                              {
+                                                  MenuId = i.ItemMenuId,
+                                                  MenuName = i.Name,
+                                                  DisplayName = i.Name,
+                                                  Controller = i.Controlador,
+                                                  Action = i.Accion,
+                                                  ParentId = i.ItemMenuPadreId,
+                                                  Icon = i.Icon
+                                              }).ToList()
+                             }).ToList().FirstOrDefault();
 
-
-                if (!CommonUtility.IsDevelopmentServer())
-                {
-                    var model = new MenuModel()
-                    {
-                        Nombre = "Leandro",
-                        Apellido = "Lopez",
-                        DefaultAction = "Create",
-                        DefaultController = "Payment",
-                        MenuItems = new List<MenuItemModel>()
-                        { new MenuItemModel()
-                            {
-                                Action = null, Controller = null,DisplayName = "Mercado Pago",MenuId = 1,MenuName = "MercadoPago",ParentId = null
-                            },
-                            new MenuItemModel()
-                            {
-                                Action = "Create", Controller = "Payment",DisplayName = "Nuevo Pago",MenuId = 1,MenuName = "NuevoPago",ParentId = 1
-                            }
-                    }};
-
-                    result = this.View(model);
-                }
-                else
-                {
-
-                    var model = (from x in _dbContext.UsuarioRol
-                                 join u in _dbContext.Usuario on x.UserId equals u.UserId
-                                 join r in _dbContext.Role on x.RolId equals r.RolId
-                                 where u.Email == user
-                                 select new MenuModel()
-                                 {
-                                     Nombre = u.Nombre,
-                                     Apellido = u.Apellido,
-                                     DefaultController = r.DefaultController,
-                                     DefaultAction = r.DefaultAction,
-                                     MenuItems = (from rm in _dbContext.RoleMenu
-                                                  join i in _dbContext.ItemMenu on rm.ItemMenuId equals i.ItemMenuId
-                                                  where rm.RolId == x.RolId
-                                                  select new MenuItemModel()
-                                                  {
-                                                      MenuId = i.ItemMenuId,
-                                                      MenuName = i.Name,
-                                                      DisplayName = i.Name,
-                                                      Controller = i.Controlador,
-                                                      Action = i.Accion,
-                                                      ParentId = i.ItemMenuPadreId,
-                                                      Icon = i.Icon
-                                                  }).ToList()
-                                 }).ToList().FirstOrDefault();
-
-                    result = this.View(model);
-
-                }
+                result = this.View(model);
 
             }
-
+            
             return result;
         }
 
