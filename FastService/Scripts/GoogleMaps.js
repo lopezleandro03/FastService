7af4d24be0cb9574cd3f47bcb06a5fd1487168bb
lopezleandro03 <lopezleandro03@gmail.com>
2018-05-30 22:20:41 -0300

    Fix broken console messages

---
+++ FastService/Scripts/GoogleMaps.js
@@ -0,0 +1,12 @@
+﻿function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById("@ViewBag.InputId")),
+        { types: ['geocode'] });
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAF44JdvJ96J5WyhIwxFRc646TTjhQ2dIY&libraries=places&callback=initAutocomplete"
+    async defer></script>
